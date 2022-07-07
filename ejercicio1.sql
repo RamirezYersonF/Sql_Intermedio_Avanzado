f94b9fcea726543d239916e998944f4ee74c2285
Yerson Felipe Ramirez <yfrg10@gmail.com>
2022-07-06 23:02:07 -0500

    Ejercicio1 y ejercicio2

--- ejercicio1.sql
+++ ejercicio1.sql
@@ -1,14 +1,15 @@
 -- EJERCICIO1 : MOSTRAR EN LA PANTALLA EL PRIMER REGISTRO.
 SELECT *
-FROM nombre_tabla.alumnos AS alumnos
+FROM nombre_bd.nombre_tabla AS alumnos
 FETCH FIRST 1 ROWS ONLY;
+-- TREAME LOS PRIMERS 1 REGISTROS DE LA FILA 
 
 SELECT *
-FROM nombre_tabla.alumnos
+FROM nombre_bd.nombre_tabla
 LIMIT 1;
 
 SELECT * FROM(
     SELECT ROW_NUMBER() OVER() AS ROW_ID
-    FROM nombre_tabla.alumnos 
+    FROM nombre_bd.nombre_tabla 
 ) AS alumn_with_row_num
 WHERE row_id = 1:
