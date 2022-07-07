df338fe1d8a3a887acad9cd0077a70baad4f431d
Yerson Felipe Ramirez <yfrg10@gmail.com>
2022-07-06 22:54:20 -0500

    EJERCICIO 1 RESUELTO

--- ejercicio1.sql
+++ ejercicio1.sql
@@ -4,11 +4,11 @@ FROM nombre_tabla.alumnos AS alumnos
 FETCH FIRST 1 ROWS ONLY;
 
 SELECT *
-FROM platzi.alumnos
+FROM nombre_tabla.alumnos
 LIMIT 1;
 
 SELECT * FROM(
     SELECT ROW_NUMBER() OVER() AS ROW_ID
-    FROM platzi.alumnos 
+    FROM nombre_tabla.alumnos 
 ) AS alumn_with_row_num
 WHERE row_id = 1:
